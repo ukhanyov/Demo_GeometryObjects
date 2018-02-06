f458eafac00c768b1381aa1fbb406098719b4961
ukhanyov <ukhanyov.oleg@gmail.com>
2018-02-06 09:16:11 +0200

    Added GeometryObject class

---
+++ src/GeometryObject.java
@@ -0,0 +1,24 @@
+public class GeometryObject {
+
+    private String color;
+    private double area;
+
+    private GeometryObject() {
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+}
