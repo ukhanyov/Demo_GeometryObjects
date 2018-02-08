5283200d746c0663307e697618646d3a2594e5da
ukhanyov <ukhanyov.oleg@gmail.com>
2018-02-08 22:47:54 +0200

    Edited the way trapezoids are generated.

--- src/shapes/GeometryObjectFactory.java
+++ src/shapes/GeometryObjectFactory.java
@@ -43,20 +43,36 @@ public class GeometryObjectFactory {
     }
 
     private static Trapezoid createTrapezoid(Colors color){
+        double generatedSmallBase;
+        double generatedLeftSide;
+        double generatedRightSide;
+        double generatedBigBase;
 
-        double generatedSmallBase = ThreadLocalRandom.current().nextDouble(
-                Constants.CONSTANT_MIN_TRAPEZOID_SIDE,
-                Constants.CONSTANT_MAX_TRAPEZOID_SIDE);
-        double generatedLeftSide = ThreadLocalRandom.current().nextDouble(
-                Constants.CONSTANT_MIN_TRAPEZOID_SIDE,
-                generatedSmallBase);
-        double generatedRightSide = ThreadLocalRandom.current().nextDouble(
-                Constants.CONSTANT_MIN_TRAPEZOID_SIDE,
-                generatedSmallBase);
+        while (true){
+            generatedSmallBase = ThreadLocalRandom.current().nextDouble(
+                    Constants.CONSTANT_MIN_TRAPEZOID_SIDE,
+                    Constants.CONSTANT_MAX_TRAPEZOID_SIDE);
+             generatedLeftSide = ThreadLocalRandom.current().nextDouble(
+                    Constants.CONSTANT_MIN_TRAPEZOID_SIDE,
+                    generatedSmallBase);
+            generatedRightSide = ThreadLocalRandom.current().nextDouble(
+                    Constants.CONSTANT_MIN_TRAPEZOID_SIDE,
+                    generatedSmallBase);
+            generatedBigBase = ThreadLocalRandom.current().nextDouble(generatedSmallBase,
+                    Constants.CONSTANT_MAX_TRAPEZOID_SIDE);
+
+            double height = Math.sqrt(((generatedBigBase + generatedLeftSide - generatedSmallBase + generatedRightSide)*
+                    (-generatedBigBase + generatedLeftSide + generatedSmallBase + generatedRightSide)*
+                    (generatedBigBase - generatedLeftSide - generatedSmallBase + generatedRightSide)*
+                    (generatedBigBase + generatedLeftSide - generatedSmallBase - generatedRightSide)) /
+                    (4 * Math.pow((generatedBigBase - generatedSmallBase),2)));
+            if(height > 0){
+                break;
+            }
+        }
 
 
-        double generatedBigBase = ThreadLocalRandom.current().nextDouble(generatedSmallBase,
-                Constants.CONSTANT_MAX_TRAPEZOID_SIDE);
+
         return new Trapezoid(generatedBigBase,
                 generatedSmallBase,
                 generatedLeftSide,
